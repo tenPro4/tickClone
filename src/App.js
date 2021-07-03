e17ca3d4c5f8d2ca9bc7b948a79831678c5ea14c
hoo ws <jiadexx@gmail.com>
2021-07-03 11:08:48 +0800

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Entry } from "./pages/entry/Entry.page";
+import { DefaultLayout } from './layout/DefaultLayout';
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+      <DefaultLayout>
+        Dashboard
+      </DefaultLayout>
     </div>
   );
 }
